bc505d79cb18cfd22bfdaf20966178f474ee249b
adnaan <foreign2009@outlook.com>
2026-05-24 22:07:25 +0200

    Fix MobileBar

--- app/components/MobileBar.tsx
+++ app/components/MobileBar.tsx
@@ -4,10 +4,7 @@ import { useCart } from "../context/CartContext";
 
 export default function MobileBar() {
 
-  const {
-    cart,
-    setCartOpen,
-  } = useCart();
+  const { cart } = useCart();
 
   return (
     <div className="fixed bottom-4 left-1/2 z-50 flex w-[92%] max-w-md -translate-x-1/2 items-center justify-between rounded-full border border-black/5 bg-white/90 px-6 py-4 shadow-2xl backdrop-blur-xl md:hidden">
@@ -30,7 +27,6 @@ export default function MobileBar() {
 
       {/* CART */}
       <button
-        onClick={() => setCartOpen(true)}
         className="rounded-full bg-black px-5 py-3 text-xs uppercase tracking-[0.2em] text-white"
       >
         Bag ({cart.length})
